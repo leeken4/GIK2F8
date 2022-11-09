a495b7ae979b6811c45072ed306990d49a2a339a
leeken4 <89828493+leeken4@users.noreply.github.com>
2022-11-09 13:40:29 +0100

    Lade till HTML för lista, JS för viss logik

---
+++ script.js
@@ -0,0 +1,50 @@
+'Use strict';
+/* Vad ska vi göra här? */
+console.log('test');
+
+const searchInput = null;
+const bookList = [
+{
+    id: 1,
+    author: 'Charles Dickens',
+    title: 'Oliver Twist'
+},
+{
+    id: 2,
+    author: 'William Shakespear',
+    title: 'Hamlet'
+}
+];
+
+function handleKeyPress(input){
+    /* Ta emot/läsa av värdert i inputfältet
+    Skicka värdet till searchBooks
+    searchBooks returnerar en filtrerad lista
+    Filtrerade listan skickas till en renderBookList
+    */
+    searchBooks(input)
+}
+
+function searchBooks(searchTerm){
+    /* Loopa igeonm bookList
+    För varje varv i loopen, ta det aktuella elementet (boken)
+    Jämför titel med söktermen
+    Om söktermen finns någonsatns i titel, lägg till elementet i ny lista (filteredList)
+    Returnerar filteredList eller anropar renderBookList?
+    */
+    const filteredList = [];
+    for(let i = 0; i < bookList.length; i++){
+        const title = bookList[i].title.toLowerCase();
+        if(title.indexOf(searchTerm.toLowerCase) >= 0){
+            filteredList.push(bookList[0])
+        }
+    }
+    renderBookList(filteredList);
+}
+
+searchBooks('oliVEr')
+
+function renderBookList(list){
+    /*Element i HTML-listan visas/döljs beroende på listans innehåll*/
+    console.log(list);
+}
