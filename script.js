842ad97c991825d2d3491f9d830de52b1c6669fa
leeken4 <89828493+leeken4@users.noreply.github.com>
2022-11-14 10:54:05 +0100

    Komponenter

--- script.js
+++ script.js
@@ -46,14 +46,9 @@ function searchBooks(searchTerm){
 
 function renderBookList(bookList){
     /*Element i HTML-listan visas/döljs beroende på listans innehåll*/
-    let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
-    for(let i = 0; i < bookList.length; i++){
-      html += `<li
-                  class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
-                ${bookList[i].author} - ${bookList[i].title}
-              </li>`;
-    }
-    html += `</ul>`;
+
+    BookList(bookList);
+
 
   const existingElement = document.querySelector('.book-list');
   console.log(existingElement)
@@ -62,8 +57,11 @@ function renderBookList(bookList){
   if (existingElement){
     root.removeChild(existingElement);
   }
+  if(bookList.length > 0){
+    root.insertAdjacentHTML('beforeend', BookList(bookList));
+  }
  
-  root.insertAdjacentHTML('beforeend', html);
+  
 }
 
 /*    <li
