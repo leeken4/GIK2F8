afd96ee18550e463180e664debd981507f392eb5
leeken4 <89828493+leeken4@users.noreply.github.com>
2022-11-14 10:33:39 +0100

    bugfix för llistsökning

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function searchBooks(searchTerm){
     const filteredList = [];
     for(let i = 0; i < bookList.length; i++){
         const title = bookList[i].title.toLowerCase();
-        if (title.indexOf(searchTerm.toLowerCase) >= 0){
+        if (title.indexOf(searchTerm.toLowerCase()) >= 0){
             filteredList.push(bookList[i])
         }
     }
@@ -47,14 +47,14 @@ function searchBooks(searchTerm){
 function renderBookList(bookList){
     /*Element i HTML-listan visas/döljs beroende på listans innehåll*/
     let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
-    for (let i = 0; i < bookList.length; i++){
-        html += `<li
-        class="book-listitem mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
-        ${bookList[i].author} - ${bookList[i].title}
-    </li>`
+    for(let i = 0; i < bookList.length; i++){
+      html += `<li
+                  class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+                ${bookList[i].author} - ${bookList[i].title}
+              </li>`;
     }
-
     html += `</ul>`;
+
   const existingElement = document.querySelector('.book-list');
   console.log(existingElement)
 
